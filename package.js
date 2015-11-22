46c32c4f0f321651e4c81e7012cfc24371cef822
Chun Yang <yang2007chun@gmail.com>
2015-11-22 12:10:08 -0600

    [sass] replace css with sass

--- package.js
+++ package.js
@@ -3,12 +3,12 @@
 Package.describe({
   name: 'poetic:materialize-scss',  // http://atmospherejs.com/materialize/materialize
   summary: 'Materialize (official): A modern responsive front-end framework based on Material Design',
-  version: '0.97.3_0',
+  version: '0.97.3_1',
   git: 'https://github.com/poetic/meteor-materialize-sass'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR@1.2');
+  api.versionsFrom('METEOR@1.0');
 
   api.use('fourseven:scss@3.3.3');
   api.use('jquery', 'client');
@@ -45,8 +45,44 @@ Package.onUse(function (api) {
 
   api.addFiles([
     'dist/js/materialize.js',
-    'dist/css/materialize.css'
   ], 'client');
+
+  var scssFiles = [
+    'sass/components/date_picker/_default.date.scss',
+    'sass/components/date_picker/_default.scss',
+    'sass/components/date_picker/_default.time.scss',
+    'sass/components/_buttons.scss',
+    'sass/components/_cards.scss',
+    'sass/components/_chips.scss',
+    'sass/components/_collapsible.scss',
+    'sass/components/_color.scss',
+    'sass/components/_dropdown.scss',
+    'sass/components/_form.scss',
+    'sass/components/_global.scss',
+    'sass/components/_grid.scss',
+    'sass/components/_icons-material-design.scss',
+    'sass/components/_materialbox.scss',
+    'sass/components/_material-icons.scss',
+    'sass/components/_mixins.scss',
+    'sass/components/_modal.scss',
+    'sass/components/_navbar.scss',
+    'sass/components/_normalize.scss',
+    'sass/components/_prefixer.scss',
+    'sass/components/_preloader.scss',
+    'sass/components/_roboto.scss',
+    'sass/components/_sideNav.scss',
+    'sass/components/_slider.scss',
+    'sass/components/_table_of_contents.scss',
+    'sass/components/_tabs.scss',
+    'sass/components/_toast.scss',
+    'sass/components/_tooltip.scss',
+    'sass/components/_typography.scss',
+    'sass/components/_variables.scss',
+    'sass/components/_waves.scss',
+    'sass/materialize.scss',
+  ];
+
+  api.addFiles(scssFiles, 'client');
 
   api.export('Materialize', 'client');
 });
