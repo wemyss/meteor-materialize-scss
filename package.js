fd8b125b40d8e830fff5c6ad09322a939e315cee
Chun Yang <yang2007chun@gmail.com>
2015-11-22 20:58:45 -0600

    1.93.3_2

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: 'poetic:materialize-scss',  // http://atmospherejs.com/materialize/materialize
   summary: 'Materialize (official): A modern responsive front-end framework based on Material Design',
-  version: '1.97.3_1',
+  version: '1.97.3_2',
   git: 'https://github.com/poetic/meteor-materialize-scss'
 });
 
